e4debf0b8481c525ab247bf09dde6693c02f835c
Shallfeu <chigvincevra@mail.ru>
2023-07-08 17:05:14 +0300

    fix: lost reference

--- src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -10,10 +10,13 @@ export const useInfiniteScroll = (props: UseInfiniteScrollOptions) => {
     const { callback, triggerRef, wrapperRef } = props;
 
     useEffect(() => {
+        const wrapperElement = wrapperRef.current;
+        const triggerElement = triggerRef.current;
+
         let observer: IntersectionObserver | null = null;
 
         if (callback) {
-            const options = { root: wrapperRef.current, rootMargin: '0px', threshold: 1.0 };
+            const options = { root: wrapperElement, rootMargin: '0px', threshold: 1.0 };
 
             observer = new IntersectionObserver(([entry]) => {
                 if (entry.isIntersecting) {
@@ -21,12 +24,12 @@ export const useInfiniteScroll = (props: UseInfiniteScrollOptions) => {
                 }
             }, options);
 
-            observer.observe(triggerRef.current);
+            observer.observe(triggerElement);
 
             return () => {
                 if (observer) {
                     // eslint-disable-next-line
-                    observer.unobserve(triggerRef.current);
+                    observer.unobserve(triggerElement);
                 }
             };
         }
